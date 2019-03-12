31d9cbf221fe9e35a027c57c5e3010ea63faf0b8
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-03-12 07:58:48 +0100

    More lambda examples

--- examples/java8/DateTimeAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/lambdaexpression/StringAnalyzer.java
+++ examples/java8/DateTimeAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/lambdaexpression/StringAnalyzer.java
@@ -4,6 +4,12 @@ package it.sunnyvale.academy.jsenewfeatures.lambdaexpression;
  *
  * @author denismaggiorotto
  */
+
+// @FunctionalInterface annotation prevents this interface to have more than one public abstract method, can be omitted
+@FunctionalInterface
 public interface StringAnalyzer {
   public boolean analyze(String target, String searchStr);
+  
+  // Uncomment to get the error "Invalid '@FunctionalInterface' annotation; StringAnalyzer is not a functional interface" at compile time
+  // public boolean analyzeMany(String[] target, String searchStr);
 }
--- examples/java8/DateTimeAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/lambdaexpression/StringAnalyzerLambda.java
+++ examples/java8/DateTimeAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/lambdaexpression/StringAnalyzerLambda.java
@@ -6,8 +6,31 @@ package it.sunnyvale.academy.jsenewfeatures.lambdaexpression;
  */
 public class StringAnalyzerLambda {
 
+
     public static void main(String[] args) {
+
+        int counter = 0;
+
+        // Compact Lambda Expression
         System.out.println(StringAnalyzerLambda.analyze(args[0], args[1], (target, searchString)->target.contains(searchString)));
+
+        // Lambda Expression with parameter type declaration
+        System.out.println(StringAnalyzerLambda.analyze(args[0], args[1], (String target, String searchString)->target.contains(searchString)));
+
+        // Multi line Lambda Expression
+        System.out.println(StringAnalyzerLambda.analyze(args[0], args[1], (String target, String searchString)-> {
+            System.out.println("Don't do this!");
+            return target.contains(searchString);
+        }));
+
+
+        System.out.println(StringAnalyzerLambda.analyze(args[0], args[1], (String target, String searchString)-> {
+            System.out.println("Don't do this!");
+            // Uncomment to get the error "Local variable counter defined in an enclosing scope must be final or effectively final" at compile time
+            // counter = counter+1;
+            return target.contains(searchString);
+        }));
+
     }
 
     public static boolean analyze(String target, String searchString, StringAnalyzer analyzer){
