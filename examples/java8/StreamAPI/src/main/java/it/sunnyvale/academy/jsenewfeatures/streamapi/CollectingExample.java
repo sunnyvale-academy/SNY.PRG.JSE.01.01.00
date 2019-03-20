957ef6c20c71abd9108f06657bc9457169c8546f
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-03-20 10:16:39 +0100

    CollectorExample updates

--- examples/java8/StreamAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/streamapi/CollectingExample.java
+++ examples/java8/StreamAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/streamapi/CollectingExample.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 
 /**
@@ -14,17 +14,17 @@ public class CollectingExample {
 
     public static void main(String[] args) {
        
-        Predicate<SalesTxn> filterCriteria1 = t -> t.getBuyerName().contains("Mario");
 
         // collect result into a list
+        Predicate<SalesTxn> filterCriteria1 = t -> t.getBuyerName().contains("Mario");
         List<SalesTxn> txnList = SalesTxn.createTxnList();
         List<SalesTxn> resultList = txnList.stream()
                 .filter(filterCriteria1)
                 .collect(Collectors.toList());
-
         for(SalesTxn txn : resultList){
             System.out.println(txn);
         }
+        System.out.println(String.format("Result as List: %s",resultList));
 
         // collect result into a map
         Function<SalesTxn,String> keyMapperFunction =  t -> t.getBuyerName();
@@ -32,13 +32,31 @@ public class CollectingExample {
         Map<String, Double> resultMap = txnList.stream()
                 .filter(filterCriteria1)
                 .collect(Collectors.toMap(keyMapperFunction, valueMapperFunction));
-        System.out.println(resultMap);
+        System.out.println(String.format("Result as Map: %s",resultMap));
 
         // collect result into a string made by buyer names with charachter "," as a delimiter
         String resultString = txnList.stream()
                 .map( t -> t.getBuyerName())
                 .collect(Collectors.joining(", "));
-        System.out.println(resultString);
+        System.out.println(String.format("Joining buyer names: %s",resultString));
+
+        // collect result as Transaction Total average
+        ToDoubleFunction<SalesTxn> toDoubleFunction = t -> new Double(t.getTransactionTotal());
+        double txnTotalAvg = txnList.stream()
+        .collect(Collectors.averagingDouble(toDoubleFunction));
+        System.out.println(String.format("Averaging Double: %.2f",txnTotalAvg));
+
+        // collect in a Map, grouping by state
+        Function<SalesTxn,String> groupingByState =  t -> t.getState();
+        Map groupingByStateMap = txnList.stream()
+        .collect(Collectors.groupingBy(groupingByState));
+        System.out.println(String.format("Grouping by state: %s",groupingByStateMap));
+
+        // collect in a Map, partitioning by Transaction Total (+/- 100)
+        Predicate<SalesTxn> partitioningPredicate = t -> t.getTransactionTotal() >= 100;
+        Map partitioningByTransactionTotalMap = txnList.stream()
+        .collect(Collectors.partitioningBy(partitioningPredicate));
+        System.out.println(String.format("Partitioning by Transaction Total +/- 100: %s",partitioningByTransactionTotalMap));
         
     }
 }
