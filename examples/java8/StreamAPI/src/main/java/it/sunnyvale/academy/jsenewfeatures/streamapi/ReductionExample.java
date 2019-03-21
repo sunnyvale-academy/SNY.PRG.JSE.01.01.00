c0cbdf476b9ea07b11dee00676b6f3e384ee67bb
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-03-21 01:20:49 +0100

    Stream examples

---
+++ examples/java8/StreamAPI/src/main/java/it/sunnyvale/academy/jsenewfeatures/streamapi/ReductionExample.java
@@ -0,0 +1,26 @@
+package it.sunnyvale.academy.jsenewfeatures.streamapi;
+
+import java.util.List;
+
+/**
+ * ReductionExample
+ */
+public class ReductionExample {
+
+    public static void main(String[] args) {
+        
+        List<SalesTxn> list = SalesTxn.createTxnList();
+
+        double total = list.stream()
+             .filter(e -> e.getState().equals("CA"))
+             .mapToDouble(SalesTxn::getTransactionTotal)
+             .sum();
+        System.out.println(String.format("Using sum(): %.2f",total));
+
+        double reduce = list.stream()
+             .filter(e -> e.getState().equals("CA"))
+             .mapToDouble(SalesTxn::getTransactionTotal)
+             .reduce(0, (sum, element) -> sum + element);
+        System.out.println(String.format("Using reduce(): %.2f",reduce));
+    }
+}
