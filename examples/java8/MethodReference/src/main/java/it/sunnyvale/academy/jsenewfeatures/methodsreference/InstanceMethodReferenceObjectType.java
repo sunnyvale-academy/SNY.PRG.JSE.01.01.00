0ea6cb6f84c5b72eba8049e3fab4702b8a5c61fc
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-07-07 15:42:42 +0200

    fix InstanceMethodReferenceObjectType

--- examples/java8/MethodReference/src/main/java/it/sunnyvale/academy/jsenewfeatures/methodsreference/InstanceMethodReferenceObjectType.java
+++ examples/java8/MethodReference/src/main/java/it/sunnyvale/academy/jsenewfeatures/methodsreference/InstanceMethodReferenceObjectType.java
@@ -22,13 +22,17 @@ public class InstanceMethodReferenceObjectType {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(5, 3, 50, 24, 40, 2, 9, 18);
 
-         // Using a lambda expression
+        // Using a lambda expression
+        System.out.println("## Using a lambda expression");
         numbers.stream()
-            .sorted((a, b) -> Integer.compare(a, b));
+            .sorted((a, b) -> Integer.compare(a, b))
+            .forEach(t -> System.out.println(t));
 
         // Using an instance method reference of an object type (ObjectType::method)
+        System.out.println("## Using an instance method reference of an object type (ObjectType::method)");
         numbers.stream()
-            .sorted(Integer::compare);
+            .sorted(Integer::compare)
+            .forEach(System.out::println);
         
     }
 
